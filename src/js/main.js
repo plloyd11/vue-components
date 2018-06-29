f1d8b1024257c49c8a90fe01fc2243659509d9b8
Peter Lloyd <plloyd11@gmail.com>
2018-06-29 17:02:15 -0400

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+const cards = [
+    {
+      front: 'The "First Computer Programmer"',
+      back: 'Ada Lovelace',
+      flipped: false,
+    },
+    {
+      front: 'Invented the "Clarke Calculator"',
+      back: 'Edith Clarke',
+      flipped: false,
+  
+    },
+    {
+      front: 'Famous World War II Enigma code breaker',
+      back: 'Alan Turing',
+      flipped: false,
+    },
+    {
+      front: 'Created satellite orbit analyzation software for NASA',
+      back: 'Dr. Evelyn Boyd Granville',
+      flipped: false,
+    },
+  ]; 
+
+  new Vue({
+    el: '#flashcard-app',
+    data: {
+      cards: cards,
+      newFront: '',
+      newBack: ''
+    },
+    methods: {
+        toggleCard: function(card) {
+            card.flipped = !card.flipped;
+        },
+        addNew: function() {
+            this.cards.push({
+                front: this.newFront,
+                back: this.newBack,
+                flipped: false
+            })
+        }
+    }
+  });
